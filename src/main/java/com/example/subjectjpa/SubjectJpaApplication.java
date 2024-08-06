023dc98135b75fe7e856cfd533c341eee6d3be5f
YachaTree <dnftksghkfkd@icloud.com>
2024-08-06 14:25:49 +0900

    초기 셋팅

---
+++ src/main/java/com/example/subjectjpa/SubjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.subjectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubjectJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectJpaApplication.class, args);
+    }
+
+}
